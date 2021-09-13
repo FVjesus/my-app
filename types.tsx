90f68944cfed5e87ee8223fb165cbc576dfec386
Fabricio <fabriciovellozo@gmail.com>
2021-09-13 01:44:55 -0300

    Implement register screen
    close #2

--- types.tsx
+++ types.tsx
@@ -21,6 +21,7 @@ export type RootStackParamList = {
   Modal: undefined;
   NotFound: undefined;
   Login: undefined;
+  Register: undefined;
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
